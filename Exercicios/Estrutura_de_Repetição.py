a218eeb64a29d50db9f209c1feae3d6092ca9550
LSHummel <lshummel13@gmail.com>
2026-04-24 11:47:38 -0300

    ex 8 e 9

---
+++ Exercicios/Estrutura_de_Repetição.py
@@ -0,0 +1,11 @@
+'''
+n = int(input("Digite um número n: "))
+
+for i in range(1, n+1):
+    if n % i == 0:
+        print(i)
+'''
+
+for i in range(2, 2001):
+    if i/i == 1 and i/1 == i:
+        print(i)
